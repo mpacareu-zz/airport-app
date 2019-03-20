856abbf051c89f2e305ff7fa1b07d3612a43716f
mcapote <mcapote250390@gmail.com>
2019-03-20 12:22:58 -0300

    updates in api for production v2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  // production: true,
-  API_ENDPOINT: 'https://airport-backend.herokuapp.com/autocomplete'
+  production: false,
+  API_ENDPOINT : 'https://airport-backend.herokuapp.com/autocomplete'
 };
 
 /*
